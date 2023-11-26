d803c983bf391f334af505f2c591ff8de2eba70f
temmie <t-ishikawa@mces.titech.ac.jp>
2023-11-26 12:10:42 +0900

    initial commit (~ tutorial §9.2)

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,12 @@
+class CommentsController < ApplicationController
+  def create
+    @article = Article.find(params[:article_id]) # まず記事を呼び出す
+    @comment = @article.comments.create(comment_params)
+    redirect_to article_path(@article)
+  end
+    
+  private
+    def comment_params
+      params.require(:comment).permit(:commenter, :body)
+    end
+end
